d817cab282c737e432bbc96db91bb84adbc299cb
Mickmart96 <micheleciceron@gmail.com>
2024-01-19 18:24:11 +0100

    first commit

---
+++ WaterApp/src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FetchWeather from './assets/components/Fetch'
+
+function App() {
+ 
+
+  return (
+    <>
+      <FetchWeather style={{backgroundColor:"opacity"}}/>
+    </>
+  )
+}
+
+export default App
