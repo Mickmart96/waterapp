f0440ddc399a6976870578d409ce6c88e175e75c
Mickmart96 <micheleciceron@gmail.com>
2024-01-24 19:55:58 +0100

    upgradeDays

--- WaterApp/src/assets/components/Fetch.jsx
+++ WaterApp/src/assets/components/Fetch.jsx
@@ -6,17 +6,43 @@ import { Container } from "react-bootstrap";
 function FetchWeather() {
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
+  const [day, setDay] = useState(null);
 
-  const handleSearch = async () => {
+  const FetchCity = async () => {
     try {
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3f64327af1492cb6b843fb2420be96e9&units=metric`
       );
       const data = await response.json();
 
-      setWeatherData(data);
+      if (response.ok) {
+        setWeatherData(data);
+        fetchForecast(); // Avvia il recupero dei dati di previsione solo se la ricerca corrente ha avuto successo
+      } else {
+        setError("Città non trovata. Riprova con un altro nome.");
+      }
     } catch (error) {
       console.error("Errore nel recupero dei dati meteorologici:", error);
+    }
+  };
+ 
+  const fetchForecast = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3f64327af1492cb6b843fb2420be96e9&lang=en&units=metric`
+      );
+      const data = await response.json();
+      setDay(data);
+    }
+   
+    catch (error) {
+      console.error("Errore nel recupero dei dati di previsione:", error);
+    }
+  };
+
+  const handleSearch = () => {
+    if (city.length > 3) {
+      FetchCity();
     }
   };
 
@@ -30,7 +56,7 @@ function FetchWeather() {
         />
       </Container>
       <Container className="d-flex justify-content-center">
-        <RisultatiMeteo city={city} weatherData={weatherData} />
+        <RisultatiMeteo city={city} weatherData={weatherData} day={day}/>
       </Container>
     </>
   );
--- WaterApp/src/assets/components/RisultatiMeteo.jsx
+++ WaterApp/src/assets/components/RisultatiMeteo.jsx
@@ -1,40 +1,95 @@
-import { CiTempHigh } from "react-icons/ci"; 
+import React from 'react';
+import { CiTempHigh } from 'react-icons/ci';
+import { WiStrongWind, WiRaindrops, WiThermometer, WiThermometerExterior, WiCloudy } from 'react-icons/wi';
+import { Row, Card, Container, Col } from 'react-bootstrap';
 
-import { WiStrongWind } from "react-icons/wi"; 
-import { WiRaindrops } from "react-icons/wi"; 
-import { WiThermometer } from "react-icons/wi"; 
-import { WiThermometerExterior } from "react-icons/wi"; 
-import { WiCloudy } from "react-icons/wi"; 
- 
-import Card from 'react-bootstrap/Card';
+const RisultatiMeteo = ({ city, weatherData, day }) => (
+  <Container fluid>
+    {weatherData && (
+      <Card className="text-center rounded-5 w-50 d-flex transparent-card mx-auto">
+        <Card.Header className="display-3 text-white">{city.toUpperCase()}</Card.Header>
+        <Card.Body className="d-flex justify-content-between flex-wrap">
+          <Card.Title className="display-2 text-white">
+            <CiTempHigh />
+            {weatherData.main.temp}°C
+          </Card.Title>
+          <div className="text-section Row">
+            <div className="text-white">
+              <p>
+                <WiThermometer style={{ fontSize: '1.7em' }} /> {weatherData.main.temp_max}°C
+              </p>
+            </div>
+            <div className="text-white">
+              <p>
+                <WiThermometerExterior style={{ fontSize: '1.7em' }} /> {weatherData.main.temp_min}°C
+              </p>
+            </div>
+            <div className="text-white">
+              <p className="text-white">
+                <WiRaindrops style={{ fontSize: '2em' }} />
+                {weatherData.main.humidity}%
+              </p>
+            </div>
+          </div>
+          <div className="text-section">
+            <p className="text-white">
+              <WiCloudy style={{ fontSize: '1.7em' }} /> {weatherData.weather[0].description}
+            </p>
+            <p className="text-white">
+              <WiStrongWind style={{ fontSize: '1.8em' }} />
+              {weatherData.wind.speed}KM/h
+            </p>
+          </div>
+        </Card.Body>
+      </Card>
+    )}
+    <br />
+    {day && (
+      <Container className='text-center'>
+        <Row>
+          <Col>
+            <div className="text-white">
+              <div className="text-section Row">
+                <h2 className="text-white">OGGI</h2>
+                <div className="text-white">
+                  <p className="h5"> <CiTempHigh style={{ fontSize: '1.7em' }}/>{day.list[0].main.temp}°C</p>
+                  <p className="h5"><WiRaindrops style={{ fontSize: '2em' }} />{day.list[0].main.humidity}%</p>
+                  <p className="h5"><WiStrongWind style={{ fontSize: '1.8em' }} />{day.list[0].wind.speed}km/h</p>
+                  <p className="h5"><WiCloudy style={{ fontSize: '1.7em' }} />{day.list[0].weather[0].description}</p>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div className="text-white">
+              <div className="text-section Row">
+                <h2 className="text-white">DOMANI</h2>
+                <div className="text-white">
+                  <p className="h5"><CiTempHigh style={{ fontSize: '1.7em' }}/>{day.list[8].main.temp}°C</p>
+                  <p className="h5"><WiRaindrops style={{ fontSize: '2em' }} />{day.list[8].main.humidity}%</p>
+                  <p className="h5"><WiStrongWind style={{ fontSize: '1.8em' }} />{day.list[8].wind.speed}km/h</p>
+                  <p className="h5"><WiCloudy style={{ fontSize: '1.7em' }} />{day.list[8].weather[0].description}</p>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div className="text-white">
+              <div className="text-section Row">
+                <h2 className="text-white">DOPODOMANI</h2>
+                <div className="text-white">
+                  <p className="h5"> <CiTempHigh style={{ fontSize: '1.7em' }}/>{day.list[16].main.temp}°C</p>
+                  <p className="h5"> <WiRaindrops style={{ fontSize: '2em' }} />{day.list[16].main.humidity}%</p>
+                  <p className="h5"><WiStrongWind style={{ fontSize: '1.8em' }} />{day.list[16].wind.speed}km/h</p>
+                  <p className="h5"><WiCloudy style={{ fontSize: '1.7em' }} />{day.list[16].weather[0].description}</p>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    )}
+  </Container>
+);
 
-
-
-const RisultatiMeteo = ({city, weatherData}) => (
-    <>
-      {weatherData && (
- <Card className="text-center rounded-5 w-50 d-flex transparent-card">
- <Card.Header className='display-3 text-white'>{city}</Card.Header>
- <Card.Body className="d-flex justify-content-between flex-wrap">
- <Card.Title className='display-2 text-white'><CiTempHigh />{weatherData.main.temp}°C</Card.Title>
-   <div className="text-section Row">
-    <div className="text-white">
-       <p ><WiThermometer style={{ fontSize: '1.7em' }}/> {weatherData.main.temp_max}°C</p>
-       <p><WiThermometerExterior style={{ fontSize: '1.7em' }}/> {weatherData.main.temp_min}°C</p>
-       <p className='text-white'><WiRaindrops style={{ fontSize: '2em' }}/>{weatherData.main.humidity}%</p>
-     </div>
-   </div>
-   <div className="text-section">
-     <p className='text-white'><WiCloudy style={{ fontSize: '1.7em' }}/> {weatherData.weather[0].description}</p>
-     <p className='text-white'><WiStrongWind style={{ fontSize: '1.8em' }}/>{weatherData.wind.speed}KM/h</p>
-   </div>
-  
- </Card.Body>
-</Card>
-      )}
-
-
-    </>
-  );
-
-  export default RisultatiMeteo;
+export default RisultatiMeteo;
